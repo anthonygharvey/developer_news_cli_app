34b9b5518aea7825ea160a3ebea076d8e53df4e4
Anthony <anthony@anthonygharvey.com>
2018-01-14 21:56:37 -0500

    Add HackerNoonScrapper class

---
+++ lib/developer_news_cli_app/HackerNoonScrapper.rb
@@ -0,0 +1,23 @@
+class DeveloperNewsCliApp::HackerNoonScrapper
+
+	def get_page
+		page = Nokogiri::HTML(open("https://hackernoon.com/"))
+	end
+
+	def get_articles
+		self.get_page.css(".js-trackedPost")
+	end
+
+	def make_article
+		self.get_articles.each do |article|
+			a = DeveloperNewsCliApp::Article.new
+			a.title = article.css("h3").text
+			a.author = article.css(".ds-link").text
+			a.date = article.css("time").text
+			a.url = article.css("a").attribute("href").value
+			a.website = "HackerNoon"
+			a.trailing = article.css(".u-fontSize18").text
+		end
+	end
+
+end
