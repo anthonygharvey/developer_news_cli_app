9da11e9a4f88cae4aea43b8439482b7e3318cdeb
Anthony <anthony@anthonygharvey.com>
2018-01-18 15:50:03 -0500

    Add AListApartScrapper class to scrape articles from https://alistapart.com/articles

---
+++ lib/developer_news_cli_app/AListApartScrapper.rb
@@ -0,0 +1,32 @@
+class DeveloperNewsCliApp::AListApartScrapper
+	@@all = []
+
+	def get_page
+		page = Nokogiri::HTML(open("https://alistapart.com/articles"))
+	end
+
+	def get_articles
+		self.get_page.css(".entry-list")
+	end
+
+	def make_article
+		self.get_articles.each do |article|
+			a = DeveloperNewsCliApp::Article.new
+			a.title = article.css(".entry-title a").text
+			a.author = article.css(".author").text
+			a.date = article.css("time").text
+			a.trailing = article.css("p")[1].text
+			a.url = "https://alistapart.com" + article.css(".entry-title a").attribute("href").value
+			@@all << a
+		end
+	end
+
+	def self.articles
+		@@all
+	end
+
+	def self.article_count
+		@@all.count
+	end
+
+end
